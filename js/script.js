f43776536cb494ebc98ffcef99b1caf6b98c0617
AlbertoOrlando <albertoorlandowork@gmail.com>
2025-01-12 20:43:00 +0100

    feat esercizio accensione spegnimento lampada al click

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const cambioLuce = document.querySelector('.luce');
+const pulsante = document.querySelector('.on_off');
+
+pulsante.addEventListener('click', function() {
+    cambioLuce.classList.toggle('accesa');
+    if (cambioLuce.classList.contains('accesa')) {
+        cambioLuce.src = "img/yellow_lamp.png";
+        pulsante.textContent = "Spegni";
+    } else {
+        cambioLuce.src = "img/white_lamp.png";
+        pulsante.textContent = "Accendi";
+    }
+});
